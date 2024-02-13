6bdb4933cfa92c89559b9d40f255cb562dd881fd
Sandeep Yadav <103882286+sandeepyadav-24@users.noreply.github.com>
2024-02-13 21:42:21 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use("/admin", adminRouter)
 app.use("/user", userRouter)
-app.get("/",(req,res)=> res.json({mes:"Hello World"}));
+app.get("/",(req,res)=> res.json({mes:"Hello World1"}));
 
 
 // Connect to MongoDB
